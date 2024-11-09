1b712acc992abf4ebc19eacc7184eaff361996d6
ThomasRosebrook <trosebr@ksu.edu>
2024-11-08 18:42:35 -0600

    Finished Lighting and Camera Example

---
+++ LightingAndCamerasExample/CirclingCamera.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace LightingAndCamerasExample
+{
+    public class CirclingCamera : ICamera
+    {
+        float angle;
+
+        Vector3 position;
+
+        float speed;
+
+        Game game;
+
+        public Matrix View { get; private set; }
+
+        public Matrix Projection { get; private set; }
+
+        /// <summary>
+        /// Constructs a new camera that circles the origin
+        /// </summary>
+        /// <param name="game">The game this camera belongs to</param>
+        /// <param name="position">The initial position of the camera</param>
+        /// <param name="speed">The speed of the camera</param>
+        public CirclingCamera(Game game, Vector3 position, float speed)
+        {
+            this.game = game;
+            this.position = position;
+            this.speed = speed;
+            this.Projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.PiOver4,
+                game.GraphicsDevice.Viewport.AspectRatio,
+                1,
+                1000
+            );
+            this.View = Matrix.CreateLookAt(
+                position,
+                Vector3.Zero,
+                Vector3.Up
+            );
+        }
+
+        /// <summary>
+        /// Updates the camera's positon
+        /// </summary>
+        /// <param name="gameTime">The GameTime object</param>
+        public void Update(GameTime gameTime)
+        {
+            // update the angle based on the elapsed time and speed
+            angle += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Calculate a new view matrix
+            this.View =
+                Matrix.CreateRotationY(angle) *
+                Matrix.CreateLookAt(position, Vector3.Zero, Vector3.Up);
+        }
+    }
+}
--- LightingAndCamerasExample/Game1.cs
+++ LightingAndCamerasExample/Game1.cs
@@ -9,6 +9,13 @@ namespace LightingAndCamerasExample
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // our crate
+        Crate[] crates;
+
+        // The camera 
+        //CirclingCamera camera;
+        FPSCamera camera;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -27,6 +34,19 @@ namespace LightingAndCamerasExample
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            // Make some crates
+            crates = new Crate[] {
+                new Crate(this, CrateType.DarkCross, Matrix.Identity),
+                new Crate(this, CrateType.Slats, Matrix.CreateTranslation(4, 0, 5)),
+                new Crate(this, CrateType.Cross, Matrix.CreateTranslation(-8, 0, 3)),
+                new Crate(this, CrateType.DarkCross, Matrix.CreateRotationY(MathHelper.PiOver4) * Matrix.CreateTranslation(1, 0, 7)),
+                new Crate(this, CrateType.Slats, Matrix.CreateTranslation(3, 0, -3)),
+                new Crate(this, CrateType.Cross, Matrix.CreateRotationY(3) * Matrix.CreateTranslation(3, 2, -3))
+            };
+
+            //camera = new CirclingCamera(this, new Vector3(0, 5, 10), 0.5f);
+            camera = new FPSCamera(this, new Vector3(0, 3, 10));
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -34,6 +54,8 @@ namespace LightingAndCamerasExample
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            camera.Update(gameTime);
 
             // TODO: Add your update logic here
 
@@ -43,6 +65,11 @@ namespace LightingAndCamerasExample
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            foreach (Crate crate in crates)
+            {
+                crate.Draw(camera);
+            }
 
             // TODO: Add your drawing code here
 
